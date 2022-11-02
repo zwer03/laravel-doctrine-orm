82005090b43be8fdf0e6a569831c8765bb479556
Nolie Francisco <nolie@aloware.com>
2022-11-02 10:00:29 +0800

    Refactor Animal Class

--- app/Entities/Animal.php
+++ app/Entities/Animal.php
@@ -6,39 +6,41 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  * @ORM\InheritanceType("JOINED")
- * @ORM\DiscriminatorColumn(name="discr", type="string")
+ * @ORM\DiscriminatorColumn(name="discriminator", type="string")
  * @ORM\DiscriminatorMap({"animal" = "Animal", "cat" = "Cat", "dog" = "Dog"})
  */
-abstract class Animal
+class Animal
 {
     /**
      * @var int
-     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $id;
+    private int $id;
 
-    /** @ORM\Column(type="string") */
-    private $color;
+    /** 
+     * @var string
+     * @ORM\Column(type="string") 
+     */
+    private string $color;
 
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function setId($id)
+    public function setId(int $id): void
     {
         $this->id = $id;
     }
 
-    public function getColor()
+    public function getColor(): string
     {
         return $this->color;
     }
 
-    public function setColor($color)
+    public function setColor(string $color): void
     {
         $this->color = $color;
     }
